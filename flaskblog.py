3f4816f39c01f194bcd392601348f6d4fedba579
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-08-25 09:54:49 -0500

    Finished part 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Devin North',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'July 12, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 14, 2019'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about_page():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 
 if __name__ == "__main__":
